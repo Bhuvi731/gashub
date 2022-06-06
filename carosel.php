9377fa7277644e468d1278a8bc0146edf849abc3
Bhuvi731 <105772345+Bhuvi731@users.noreply.github.com>
2022-06-06 16:25:29 +0530

    Update carosel.php

--- carosel.php
+++ carosel.php
@@ -30,7 +30,7 @@ if(isset($_POST['submit']))
 {
   $fileName=$_FILES['file']['name'];
   $tmpName=$_FILES['file']['tmp_name'];
-  $UploadDir='http://gashub.amicodevelopment.net:5432/uploads/banners/';
+  $UploadDir='http://gashub.amicodevelopment.net/uploads/banners/';
   $filePath=$UploadDir.$fileName;
   $result = move_uploaded_file($tmpName, $filePath); 
   if($result){
