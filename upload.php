9a6de13cbb78ea9f9d46023ac92af54761ea6e6a
Bhuvi731 <105772345+Bhuvi731@users.noreply.github.com>
2022-06-07 18:41:10 +0530

    Update upload.php

--- upload.php
+++ upload.php
@@ -7,8 +7,8 @@ $backlink='<a href="./">Go back</a>';
 $tagetDir='http://gashub.amicodevelopment.net/uploads/banners/';
 
 $fileName=basename($_FILES['file']['name']);
- echo $targetfilePath=$tagetDir .$fileName;
- $filetype= pathinfo($targetfilePath,PATHINFO_EXTENSION);
+$targetfilePath=$tagetDir .$fileName;
+$filetype= pathinfo($targetfilePath,PATHINFO_EXTENSION);
 
 if(isset($_POST['submit']) && !empty($_FILES['file']['name']))
 {
