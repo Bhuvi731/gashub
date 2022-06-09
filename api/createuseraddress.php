664e8f4b22ec0bee49e1edabad0f9e8a0c3bae2b
arokiaani2423 <arokianani2423@gmail.com>
2022-06-09 15:19:13 +0530

    Update createuseraddress.php

--- api/createuseraddress.php
+++ api/createuseraddress.php
@@ -20,10 +20,10 @@ $country=$_POST['country'];
 $pincode=$_POST['pincode'];
 $latitude=$_POST['latitude'];
 $longitude=$_POST['longitude'];
-$status=$_POST['status'];
+$status="1";
 $createdby="1";
-if(!empty($name) && !empty($addressline1) &&  !empty($addressline2) && 
-!empty($city) && !empty($landmark) && !empty($district) &&  !empty($state) &&  !empty($country) && !empty($pincode) && !empty($latitude) && !empty($longitude) &&
+if(!empty($name) && !empty($addressline1) && 
+!empty($city) && !empty($landmark) && !empty($district) &&  !empty($state) &&  !empty($country) && !empty($pincode)  &&
 !empty($status)){ 
 
         $userrow= pg_query($db,"select * from users order by id desc");
@@ -33,20 +33,22 @@ if(!empty($name) && !empty($addressline1) &&  !empty($addressline2) &&
         
         $sql=pg_query($db,"INSERT INTO useraddresses(userid,name,addressline1,addressline2,city,landmark,district,state,country,pincode,latitude,longitude,status,createdby)VALUES('$user_id','$name','$addressline1','$addressline2','$city','$landmark','$district','$state','$country','$pincode','$latitude','$longitude','$status','$createdby')");
         
-        http_response_code(201);  
+        if($sql)
+    {
+       
+        http_response_code(201);         
         echo json_encode(array("message" => "Successfull"));
-         }
-        
-    else{
+      
+    }else
+    {
         http_response_code(503);        
         echo json_encode(array("message" => "Error"));
-       }
-       
-     
-    // else
-    //    {
-    //      http_response_code(400);    
-    //      echo json_encode(array("message" => "Error Please Check."));
-    //    } 
-  
+    }
+}
+else
+{
+    http_response_code(400);    
+    echo json_encode(array("message" => "Error Please Check."));
+}
+
 ?>
