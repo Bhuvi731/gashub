5e65a54d0ef905c40d8ac44d2b3be5ef9bed5db5
Bhuvi731 <105772345+Bhuvi731@users.noreply.github.com>
2022-06-09 15:31:38 +0530

    Revert "Update deletecylinder.php"
    This reverts commit 2f98e01eaa3b5a658092b23f52129a6ab8bb38bd.

--- api/deletecylinder.php
+++ api/deletecylinder.php
@@ -1,29 +1,31 @@
 <?php
-// header("Access-Control-Allow-Origin: *");
-// header('Content-Type: text/plain');
-// header("Content-Type: application/json; charset=UTF-8");
-// header("Access-Control-Allow-Methods: POST");
-// header("Access-Control-Max-Age: 3600");
-// header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header("Access-Control-Allow-Origin: *");
+header('Content-Type: text/plain');
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 include_once '../database/db.php';
 
 
- if(isset($_REQUEST['deleteid']))
+ if(isset($_POST['deleteid']))
  {
-     $id = $_REQUEST['deleteid'];
-     echo $id;
-     echo"UPDATE cylinderweight SET status='-1' WHERE id=$id";
-	$sql=pg_query($db,"UPDATE cylinderweight SET status='-1' WHERE id=$id");
+     $id = $_POST['deleteid'];
+	
+	$sql=pg_query($db,"UPDATE cylinderweight SET status='-1' WHERE id='$id'");
 	if($sql)
-	{    
-        echo "Successfull";
+	{
+        $sql=pg_query($db,"UPDATE cylindertype SET status='-1' WHERE id='$id'");
+		http_response_code(201);         
+        echo json_encode(array("message" => "Successfull"));
 	}else
     {
-        
-        echo "Error";
+        http_response_code(503);        
+        echo json_encode(array("message" => "Error"));
     }
- }
+}
  else{
- 	    echo "error";
-     }
+	echo json_encode(array("message" => "error"));
+}
+
 ?>
