bee5d9cb9bbac61625cdcc5fb78a9ec8e2d0ca08
arokiaani2423 <arokianani2423@gmail.com>
2022-06-09 14:19:14 +0530

    Update product.php

--- product.php
+++ product.php
@@ -1,3 +1,4 @@
+<?php include("database/db.php");?>
 <link rel="stylesheet" href="assets/datatables-bs4/css/dataTables.bootstrap4.min.css">
 <link rel="stylesheet" href="assets/datatables-responsive/css/responsive.bootstrap4.min.css">
 <link rel="stylesheet" href="assets/datatables-buttons/css/buttons.bootstrap4.min.css">
@@ -56,7 +57,7 @@
 <div class="form-group">
 <label for="exampleSelectBorder">Status</label>
 <select class="custom-select" id="status" name="status">
-<option>Status</option>
+<option value="">Status</option>
 <option value="1">Active</option>
 <option value="2">InActive</option>
 </select>
@@ -77,7 +78,6 @@
 </div>
 
 </div>
-
 </section>
 <section class="content" id="content">
 <div class="container-fluid">
@@ -89,6 +89,12 @@
 </div>
 
 <div class="card-body">
+<section>
+<div style="margin:auto;">
+   <label style="color: #0054A8;" for="">SEARCH :</label>
+<input id="myInput" type="text" placeholder="Search..">
+</div>
+</section>
 <table id="example2" class="table table-bordered table-hover">
 <thead>
 <tr>
@@ -103,20 +109,22 @@
 <th>Action</th>
 </tr>
 </thead>
-<tbody>
+<tbody id="myTable">
 <?php
-$vendor=pg_query($db,"SELECT * FROM products LEFT JOIN vendors on vendors.id=products.vendorid LEFT JOIN cylindertype on cylindertype.id=products.cylindertypeid LEFT JOIN cylinderweight on cylinderweight.id=products.cylinderweightid WHERE products.status=1");
+$vendor=pg_query($db,"SELECT products.id,vendors.businessname,products.location,cylindertype.type,cylinderweight.weight,products.price,products.status,products.createdby from products LEFT JOIN vendors ON vendors.id=products.vendorid LEFT JOIN cylindertype ON cylindertype.id=products.cylindertypeid LEFT JOIN cylinderweight ON cylinderweight.id=products.cylinderweightid where products.status=1");
 $i=1;
 while($row=pg_fetch_assoc($vendor))
 {
 	?>
 <tr>
 <td><?php echo $i?></td>
-<td><?php echo $row['businessname'] ?></td>
+<td><?php echo $row['businessname']?></td>
 <td><?php echo $row['location'] ?></td>
-<td><?php echo $row['type'] ?></td>
+<td><?php echo $row['type']?></td>
 <td><?php echo $row['weight'] ?></td>
 <td><?php echo $row['price'] ?></td>
+
+
 <td>
 <?php
 if($row['status']==1)
@@ -151,7 +159,7 @@ if($row['createdby']==1)
 </div>
 <div class="modal-footer justify-content-between">
 <button type="button" class="btn btn-default" data-dismiss="modal">No</button>
-<button type="button" class="btn btn-primary" onclick = "deleterecord(<?php echo $row['id'];?>)" >Yes</button>
+<button type="button" class="btn btn-primary" onclick="deleterecord(<?php echo $row['id'];?>)">Yes</button>
 </div>
 </div>
 
@@ -170,6 +178,12 @@ if($row['createdby']==1)
 </div>
 <div class="modal-body">
 <form>
+<div class="card-body">
+
+<div class="form-group">
+<label for="exampleInputEmail1">Vendor Name</label>
+<input type="text" class="form-control" id="businessname<?php echo $row['id'];?>" placeholder="Enter Vendor Name" name="businessname" value="<?php echo $row['businessname'];?>">
+</div>
 <div class="card-body">
 
 <div class="form-group">
@@ -217,7 +231,29 @@ if($row['createdby']==1)
 <div class="container-fluid">
 <div class="card-body">
 <div class="row">
+<div class="col-sm-4">
+<div class="form-group">
+<label for="exampleInputEmail1">Vendor Name</label>
+</div>
+</div>
+<div class="col-sm-8">
+<div class="form-group">
+<p class="text-sm"><?php echo $row['businessname'];?>
+</p>
+</div>
+</div>
 
+<div class="col-sm-4">
+<div class="form-group">
+<label for="exampleInputEmail1">Vendor Name</label>
+</div>
+</div>
+<div class="col-sm-8">
+<div class="form-group">
+<p class="text-sm"><?php echo $row['businessname'];?>
+</p>
+</div>
+</div>
 <div class="col-sm-4">
 <div class="form-group">
 <label for="exampleInputEmail1">Location</label>
@@ -288,7 +324,6 @@ if($row['createdby']==1)
 </section>
 
 <script src="plugins/jquery/jquery.min.js"></script>
-
 <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <script src="plugins/jszip/jszip.min.js"></script>
 <script src="plugins/pdfmake/pdfmake.min.js"></script>
@@ -300,6 +335,17 @@ if($row['createdby']==1)
 <script src="plugins/sweetalert2/sweetalert2.min.js"></script>
 
 <script src="plugins/toastr/toastr.min.js"></script>
+<script>
+$(document).ready(function(){
+  $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#myTable tr").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
+</script>
+
 <script>
   $(function () {
     
@@ -313,61 +359,58 @@ if($row['createdby']==1)
       "responsive": true,
     });
   });
-</script>
+</script> 
 <script>
-  
-  
-  
   function save()
   {
-    var location=$("#location").val();
-    var price=$("#price").val();
-    var status=$("#status").val();
-    if ( location == "") {
-      alert("location must be filled out");
+    
+ var location=$("#location").val();
+ var price=$("#price").val();
+var status=$("#status").val();
+  if ( location == "") {
+     alert("location must be filled out");
+      return false;
+     }
+ if ( price == "") {
+        alert("price must be filled out");
       return false;
     }
-    if ( price == "") {
-      alert("price must be filled out");
-      return false;
-    }
-    else if(status == "Status")
-    {
-      alert("Status must be filled out");
-      return false;
-    }else if(location !== "" && price !== "" && status !== "" )
-    {
-      $.ajax({
-      url:"http://localhost:8080/gash/api/createproduct",
-      method:"POST",
-      dataType: "json",
-      data: {
+    else if(status == "")
+    {  
+         alert("Status must be filled out");
+       return false;
+     }else if(location !== "" && price !== "" && status !== "" )
+  {
+     $.ajax({
+       url:"api/createproduct.php",
+       method:"POST",
+       dataType:"json",
+       data: {
         
         "location":location,
         "price":price,
         "status":status,
       },
       success:function(msg)
-      {
+      {    
         console.log(msg);
         var message=msg['message'];
+        // alert(message);
         if(message=="Successfull")
         {
+
            success();  
-        }
-        else if(message=="email_existed")
-        {
-          email_existed();  
         }
         else{
           error();
         }
-      }
+       }
     })
     }
-    
-    
   }
+  
+  
+
   function RefreshTable() {
   
        $( "#content" ).load( "index.php?pageid=1 #content" );
@@ -409,61 +452,37 @@ if($row['createdby']==1)
       }, 1000);
           
   }
-  function email_existed()                                       
-  {
-    var Toast = Swal.mixin({
-      toast: true,
-      position: 'top-end',
-      showConfirmButton: false,
-      timer: 5000
-    });
-    Toast.fire({
-            icon: 'info',
-            title: 'Email already existed.'
-          })
-          setTimeout(function () {
-        //alert('Reloading Page');
-        location.reload(true);
-      }, 1000);
-          //window.location.reload();
-  }
-
 
   function editsave(id)
   {
-    var courseid=id;
-    var course = $("#course"+id).val();
+    
+    var location= $("#location"+id).val();
+    var price = $("#price"+id).val();
     var status = $("#status"+id).val();
-    var course_credit_hour = $("#course_credit_hour"+id).val();
-    var course_code = $("#course_code"+id).val();
-    if (course == "") {
-      alert("Name must be filled out");
+    if (location == "") {
+      alert("location must be filled out");
       return false;
     }
-    else if(status == "Status")
+    else if(status == "")
     {
       alert("Status must be filled out");
       return false;
     }
-    if (course_credit_hour == "") {
-      alert("Hour must be filled out");
+    if (price == "") {
+      alert("price must be filled out");
       return false;
     }
-    if (course_code == "") {
-      alert("Code must be filled out");
-      return false;
-    }else if(course != "" && status !="" && course_credit_hour !=="" && course_code !=="")
+    }else if(location != "" && status !="" && price !=="" )
     {
       $.ajax({
-      type:"GET",
-      url:"mastercourse.php",
+      type:"POST",
+      url:"api/createproduct.php",
       data:
       {
         "id":courseid,
-        "course":course,
+        "location":location,
+        "price":price,
         "status":status,
-        "course_credit_hour":course_credit_hour,
-        "course_code":course_code,
       },
       success:function(msg)
       {
@@ -480,7 +499,7 @@ if($row['createdby']==1)
       }
     })
     }
-  }
+  
 
   function editsuccess()
   {
@@ -503,34 +522,23 @@ if($row['createdby']==1)
 
   function deleterecord(id)
   {
-   
+    
     var deleteid=id;
+     alert(deleteid);
     $.ajax({
       type:"POST",
-      url:"http://localhost:8080/gash/api/deleteproduct",
+      url:"api/deleteproduct.php",
       data:
       {
-        
-         "deleteid":deleteid,
+        "deleteid":deleteid,
        
       },
-      success:function(msg)
-      {
-        console.log(msg);
-        var message=msg['message'];
-        
-        if(message=="Successfull")
-        {
-           
-          deletesuccess();
-          
-          
-        }else{
-          error();
-        }
+      success:function(msg){
+      console.log(msg);
       }
     })
   }
+
   function deletesuccess()
   {
     var Toast = Swal.mixin({
@@ -541,14 +549,14 @@ if($row['createdby']==1)
     });
     Toast.fire({
             icon: 'success',
-            title: 'Deleted Successfully.'
+            title: 'Program Delete Successfully.'
           })
           setTimeout(function () {
         
         location.reload(true);
       }, 1000);
-          
   }
+  
 </script>
 
 
