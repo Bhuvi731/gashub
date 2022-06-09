1ea7fb34ba741af963fc280917a08ebae36d6d06
arokiaani2423 <arokianani2423@gmail.com>
2022-06-09 15:18:01 +0530

    Update createuser.php

--- api/createuser.php
+++ api/createuser.php
@@ -14,26 +14,22 @@ $email=$_POST['email'];
 $password=$_POST['password'];
 $gender=$_POST['gender'];
 $dateofbirth=$_POST['dateofbirth'];
-$status="1";
-$createdby="1";                                                                                                                                                                                           
-if(!empty($email) &&
-!empty($password)&&
-!empty($status)){ 
-    $sql = "SELECT * FROM users WHERE email='$email'";
+$status=$_POST['status'];
+$createdby="1";                                                                                                                                                                                         
+if(!empty($firstname) && !empty($phone) && !empty($email) &&
+!empty($gender) && !empty($dateofbirth) &&
+!empty($status) && !empty($password))
+{ 
+    echo $sql = "SELECT * FROM users WHERE email='$email'";
     $res = pg_query($db, $sql);
     if(pg_num_rows($res) > 0){
         http_response_code(201);         
         echo json_encode(array("message" => "email_existed"));
-      } 
-    else{
-        
+      }else{
     $sql = "INSERT INTO users(firstname,lastname,phone,email,gender,dateofbirth,status,createdby,password)VALUES('$firstname','$lastname','$phone','$email','$gender','$dateofbirth','$status','$createdby','$password')RETURNING id";
-    
     $query=pg_query($db,$sql);
     if($query)
     {
-       $insert_row = pg_fetch_row($query);
-        $insert_id = $insert_row[0];
         $sql=pg_query($db,"INSERT INTO userlogins(userid)VALUES('$insert_id')");
         http_response_code(201);         
         echo json_encode(array("message" => "Successfull"));
@@ -42,11 +38,10 @@ if(!empty($email) &&
         http_response_code(503);        
         echo json_encode(array("message" => "Error"));
     }
-}
-}
-else
-{
+  }
+ }
+ else{
     http_response_code(400);    
     echo json_encode(array("message" => "Error Please Check."));
-}
-?>
+   }
+  ?>
