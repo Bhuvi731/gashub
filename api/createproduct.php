cd581544ff76872afd0432762dd2ed76cad663bc
arokiaani2423 <arokianani2423@gmail.com>
2022-06-09 15:17:16 +0530

    Update createproduct.php

--- api/createproduct.php
+++ api/createproduct.php
@@ -6,17 +6,22 @@ header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 include_once '../database/db.php';
+
 $location=$_POST['location'];
 $price=$_POST['price'];
 $status=$_POST['status'];
 $createdby="1";
+ 
 if(!empty($location) && !empty($price) &&
-!empty($status)){ 
+!empty($status))
+{ 
 
     $vendorrow= pg_query($db,"select * from vendors order by id desc");
     $vendorarray=pg_fetch_array($vendorrow);
     $vendor_id=$vendorarray['id'];
-
+    // echo "$vendor_id";
+    
+    
     $cylindertyrow= pg_query($db,"select * from cylindertype order by id desc");
     $cylindertyarray=pg_fetch_array($cylindertyrow);
     $cylindertype_id=$cylindertyarray['id'];
@@ -25,8 +30,8 @@ if(!empty($location) && !empty($price) &&
     $weightarray=pg_fetch_array($weightrow);
     $weight_id=$weightarray['id'];
  
-    $sql=pg_query($db,"INSERT INTO products(vendorid,cylindertypeid,cylinderweightid,location,price,status,createdby)VALUES( '$vendor_id', '$cylindertype_id','$weight_id','$location','$price','$status','$createdby')");
-    if($sql)
+     $sql=pg_query($db,"INSERT INTO products(vendorid,cylindertypeid,cylinderweightid,location,price,status,createdby)VALUES( '$vendor_id', '$cylindertype_id','$weight_id','$location','$price','$status','$createdby')");
+  if($sql)
     {
        
         http_response_code(201);         
