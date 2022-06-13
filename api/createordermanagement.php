a675af13389b2894c481c77950e9d107326aebc9
Bhuvi731 <105772345+Bhuvi731@users.noreply.github.com>
2022-06-13 11:48:26 +0530

    Update createordermanagement.php

--- api/createordermanagement.php
+++ api/createordermanagement.php
@@ -6,44 +6,32 @@ header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 include_once '../database/db.php';
-$refiltype=$_POST['refiltype'];
-$quantity=$_POST['quantity'];
-$status=$_POST['status'];
-$createdby="1";
-if(!empty($quantity) && 
-!empty($status)){ 
+if(isset($_POST['userid']))
+{
+  $userid=$_POST['userid'];
+  $deliveryaddressid=$_POST['deliveryaddressid'];
+  $vendorid=$_POST['vendorid'];
+  $productid=$_POST['productid'];
+  $refiltype=$_POST['refiltype'];
+  $quantity=$_POST['quantity'];
+  $createdby="1";
+  $createdat = date("d-m-Y");
+  $status="1";
 
-    $vendorrow= pg_query($db,"select * from users order by id desc");
-    $vendorarray=pg_fetch_array($vendorrow);
-    $userid=$vendorarray['id'];
-
-   $cylindertyrow= pg_query($db,"select * from useraddresses order by id desc");
-    $cylindertyarray=pg_fetch_array($cylindertyrow);
-    $deliveryaddressid=$cylindertyarray['id'];
-
-    $cylinderweirow= pg_query($db,"select * from vendors order by id desc");
-    $cylinderarray=pg_fetch_array($cylinderweirow);
-    $vendorid=$cylinderarray['id'];
-
-    $cylinderweightrow= pg_query($db,"select * from products order by id desc");
-    $cylinderweightarray=pg_fetch_array($cylinderweightrow);
-    $productid=$cylinderweightarray['id'];
+if(!empty($userid) &&!empty($deliveryaddressid) &&!empty($vendorid) &&!empty($productid) &&!empty($refiltype) && !empty($quantity) &&
+!empty($status)){
  
-    $sql=pg_query($db,"INSERT INTO ordermanagement(userid,deliveryaddressid,vendorid,productid,quantity,status,createdby)VALUES('$userid','$deliveryaddressid','$vendorid','$productid','$quantity','$status','$createdby')");
-    if($sql)
-    {
+    $sql=pg_query($db,"INSERT INTO ordermanagement(userid,deliveryaddressid,vendorid,productid,quantity,status,createdby,createdat,refiltype)VALUES('$userid','$deliveryaddressid','$vendorid','$productid','$quantity','$status','$createdby','$createdat','$refiltype')");
+    if($sql){
        
         http_response_code(201);         
         echo json_encode(array("message" => "Successfull"));
       
-    }else
-    {
+    }else{
         http_response_code(503);        
         echo json_encode(array("message" => "Error"));
     }
-}
-else
-{
+  }}else{
     http_response_code(400);    
     echo json_encode(array("message" => "Error Please Check."));
 }
