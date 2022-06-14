d6af824c9bd7f72c5b0fb41fc37cd5a51f45d4eb
arokiaani2423 <arokianani2423@gmail.com>
2022-06-14 15:22:38 +0530

    Update viewfeedback.php

--- api/viewfeedback.php
+++ api/viewfeedback.php
@@ -6,13 +6,13 @@ header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 include_once '../database/db.php';
-if(isset($POST['id']))
+if(isset($_POST['id']))
 {
-    $id = $POST['id'];
+    $id = $_POST['id'];
 }
+echo "SELECT feedbacks.id,feedbacks.orderid,feedbacks.userid,feedbacks.vendorid,feedbacks.vendorbranch,feedbacks.review,feedbacks.rating,feedbacks.createdat,feedbacks.petroleum_id,petroleum.petroleum_name,vendors.businessname from feedbacks LEFT JOIN petroleum ON feedbacks.petroleum_id=petroleum.id LEFT JOIN vendors ON vendors.id=feedbacks.vendorid where feedbacks.status=1 and feedbacks.userid= $id";
+$sql=pg_query($db,"SELECT feedbacks.id,feedbacks.orderid,feedbacks.userid,feedbacks.vendorid,feedbacks.vendorbranch,feedbacks.review,feedbacks.rating,feedbacks.createdat,feedbacks.petroleum_id,petroleum.petroleum_name,vendors.businessname from feedbacks LEFT JOIN petroleum ON feedbacks.petroleum_id=petroleum.id LEFT JOIN vendors ON vendors.id=feedbacks.vendorid where feedbacks.status=1 and feedbacks.userid= $id");
 
- echo "SELECT feedbacks.id,feedbacks.orderid,feedbacks.userid,feedbacks.vendorid,feedbacks.vendorbranch,feedbacks.review,feedbacks.rating,feedbacks.createdat,feedbacks.petroleum_id,petroleum.petroleum_name,vendors.businessname from feedbacks LEFT JOIN petroleum ON feedbacks.petroleum_id=petroleum.id LEFT JOIN vendors ON vendors.id=feedbacks.vendorid where feedbacks.status=1 and feedbacks.userid= $id";
-$sql=pg_query($db,"SELECT feedbacks.id,feedbacks.orderid,feedbacks.userid,feedbacks.vendorid,feedbacks.vendorbranch,feedbacks.review,feedbacks.rating,feedbacks.createdat,feedbacks.petroleum_id,petroleum.petroleum_name,vendors.businessname from feedbacks LEFT JOIN petroleum ON feedbacks.petroleum_id=petroleum.id LEFT JOIN vendors ON vendors.id=feedbacks.vendorid where feedbacks.status=1 and feedbacks.userid=$id");
 // SELECT review.feedbacks,rating.feedbacks,createdat.feedbacks from feedbacks LEFT JOIN petroleum ON feedbacks.petroleum_id=petroleum.id where feedbacks.status=1
 $my=array();
  if($sql){ 
@@ -25,6 +25,7 @@ $my=array();
                 echo json_encode($my);
            }else{
           http_response_code(503);        
-        echo json_encode(array("message" => "sucess"));
+        echo json_encode(array("message" => "Error"));
              }
+            
 ?> 
