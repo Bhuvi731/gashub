e2e9f76b4eb3d7a08b78949caa99129811e1bc0f
Bhuvi731 <105772345+Bhuvi731@users.noreply.github.com>
2022-06-14 14:37:11 +0530

    Update createcomplaints.php

--- api/createcomplaints.php
+++ api/createcomplaints.php
@@ -6,7 +6,7 @@ header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 include_once '../database/db.php';
-if(isset($_POST['userid']) && isset($_POST['orderid'])&& isset($_POST['vendorid'])){
+if(isset($_POST['userid']) && isset($_POST['orderid'])&& isset($_POST['vendorid']) && isset($_POST['petroleumid'])){
 
 $userid=$_POST['userid'];
 $petroleumid=$_POST['petroleumid'];
@@ -14,13 +14,16 @@ $orderid=$_POST['orderid'];
 $vendorid=$_POST['vendorid'];
 $vendorbranch=$_POST['vendorbranch'];
 $title=$_POST['title'];
+$com_img=$_POST['com_img'];
 $complaints=$_POST['complaints'];
 $status="1";
 $createdby="1";
-if(!empty($userid) && !empty($orderid) && !empty($vendorid) && !empty($vendorbranch) && !empty($rating) && !empty($review) &&
-!empty($status)){ 
- 
-    $sql=pg_query($db,"INSERT INTO complaints(userid,petroleumid,orderid,vendorid,vendorbranch,title,complaints,status,createdby)VALUES('$userid','$petroleumid','$orderid','$vendorid','$vendorbranch','$title','$complaints','$status','$createdby')");
+$createdat = date("d-m-Y");
+
+if(!empty($userid) && !empty($orderid) && !empty($vendorid) && !empty($vendorbranch) && !empty($title) && !empty($complaints) &&
+!empty($status) && !empty($createdat)){ 
+
+    $sql=pg_query($db,"INSERT INTO complaints(userid,petroleumid,orderid,vendorid,vendorbranch,title,complaints,status,createdat,com_img)VALUES('$userid','$petroleumid','$orderid','$vendorid','$vendorbranch','$title','$complaints','$status','$createdat','$com_img')");
     if($sql)
     {
        
