0755c43d3d93c1b94641e06efaca3d51bc7f2a86
arokiaani2423 <arokianani2423@gmail.com>
2022-06-15 11:59:55 +0530

    Update updateuser.php

--- api/updateuser.php
+++ api/updateuser.php
@@ -2,28 +2,29 @@
 header("Access-Control-Allow-Origin: *");
 header('Content-Type: text/plain');
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Methods:GET");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 include_once '../database/db.php';
 
-if(isset($_POST['id']))
+if(isset($_GET['id']))
 {
 
-$id = $_POST['id'];
-$firstname=$_POST['firstname'];
-$lastname=$_POST['lastname'];
-$phone=$_POST['phone'];
-$email=$_POST['email'];
-$gender=$_POST['gender'];
-$dateofbirth=$_POST['dateofbirth'];
-$status=$_POST['status'];
+$id = $_GET['id'];
+$firstname=$_GET['firstname'];
+$lastname=$_GET['lastname'];
+$phone=$_GET['phone'];
+$email=$_GET['email'];
+$gender=$_GET['gender'];
+$password=$_GET['password'];
+$dateofbirth=$_GET['dateofbirth'];
+$status=$_GET['status'];
 $createdby="1";                                                                                                                                                                                           
 if(!empty($firstname) && !empty($phone) && !empty($email) &&
 !empty($gender) && !empty($dateofbirth) &&
-!empty($status)){ 
+!empty($status) && !empty($password)){ 
  
-    $sql = "UPDATE users SET firstname='$firstname',lastname='$lastname',gender='$gender',phone='$phone',email='$email',dateofbirth='$dateofbirth',status='$status' WHERE id='$id'";
+    $sql = "UPDATE users SET firstname='$firstname',lastname='$lastname',gender='$gender',phone='$phone',email='$email',dateofbirth='$dateofbirth',password='$password',status='$status' WHERE id='$id'";
  $query=pg_query($db,$sql);
     if($query)
     {
