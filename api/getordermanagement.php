5621759f374552438f6217a6df9c44c404de000e
Bhuvi731 <105772345+Bhuvi731@users.noreply.github.com>
2022-06-18 10:43:08 +0530

    Update getordermanagement.php

--- api/getordermanagement.php
+++ api/getordermanagement.php
@@ -6,8 +6,10 @@ header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 include_once '../database/db.php';
-
-$sql=pg_query($db,"SELECT ordermanagement.id,ordermanagement.userid,ordermanagement.deliveryaddressid,ordermanagement.vendorid,ordermanagement.productid,ordermanagement.quantity,ordermanagement.status,ordermanagement.createdby,ordermanagement.createdat,ordermanagement.updatedby,ordermanagement.updatedat,ordermanagement.refiltype,ordermanagement.deldate,ordermanagement.cylinderimg,users.firstname,useraddresses.addressline1,useraddresses.pincode,useraddresses.latitude,useraddresses.longitude,vendors.businessname,products.cylindertypeid,products.cylinderweightid,products.location,products.price,products.petroleumid,products.proimg FROM ordermanagement left join users on users.id=ordermanagement.userid left join useraddresses on useraddresses.id=ordermanagement.deliveryaddressid left join vendors on vendors.id=ordermanagement.vendorid left join products on products.id=ordermanagement.productid where ordermanagement.status=1");
+if(isset($_POST['id']))
+{
+  $userid=$_POST['id'];
+$sql=pg_query($db,"SELECT ordermanagement.id,ordermanagement.userid,ordermanagement.deliveryaddressid,ordermanagement.vendorid,ordermanagement.productid,ordermanagement.quantity,ordermanagement.status,ordermanagement.createdby,ordermanagement.createdat,ordermanagement.updatedby,ordermanagement.updatedat,ordermanagement.refiltype,ordermanagement.deldate,ordermanagement.cylinderimg,users.firstname,useraddresses.addressline1,useraddresses.pincode,useraddresses.latitude,useraddresses.longitude,vendors.businessname,products.cylindertypeid,products.cylinderweightid,products.location,products.price,products.petroleumid,products.proimg FROM ordermanagement left join users on users.id=ordermanagement.userid left join useraddresses on useraddresses.id=ordermanagement.deliveryaddressid left join vendors on vendors.id=ordermanagement.vendorid left join products on products.id=ordermanagement.productid where ordermanagement.status=1 AND ordermanagement.userid='$userid'");
 $my=array();
  if($sql){
    //var_dump($sql); 
@@ -19,7 +21,11 @@ $my=array();
              }      
                 echo json_encode($my);
            }else{
-          http_response_code(503);        
+          http_response_code(400);        
         echo json_encode(array("message" => "Error"));
+             }
+           }else{
+          http_response_code(503);        
+        echo json_encode(array("message" => "Error please check."));
              }
 ?> 
