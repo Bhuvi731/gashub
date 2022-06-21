e5d3f5c5132ffe7b0f16eaf8a521f2d7e226fa1e
Bhuvi731 <105772345+Bhuvi731@users.noreply.github.com>
2022-06-21 12:20:10 +0530

    Update createordermanagement.php

--- api/createordermanagement.php
+++ api/createordermanagement.php
@@ -21,7 +21,7 @@ if(isset($_POST['id']))
   $cylinderimg=$_POST['cylinderimg'];
   $createdby="1";
   $createdat = date("y-m-d");
-  $status="1";
+  $status=$_POST['status'];
 
 if(!empty($userid) &&!empty($deliveryaddressid) &&!empty($productid) &&!empty($vendorid) && !empty($refiltype) && !empty($quantity) && !empty($status) &&  
   !empty($addresstype)){
