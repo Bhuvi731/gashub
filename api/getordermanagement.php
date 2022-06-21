818a00e8a684c8ccdfe2dad9d1541b4084d185fd
Bhuvi731 <105772345+Bhuvi731@users.noreply.github.com>
2022-06-21 10:56:51 +0530

    ordermanagement group

--- api/getordermanagement.php
+++ api/getordermanagement.php
@@ -9,10 +9,18 @@ include_once '../database/db.php';
 if(isset($_POST['id']))
 {
   $userid=$_POST['id'];
+  $status=$_POST['status'];
+  if($status==1){
 $sql=pg_query($db,"SELECT ordermanagement.id,ordermanagement.userid,ordermanagement.deliveryaddressid,ordermanagement.vendorid,ordermanagement.productid,ordermanagement.quantity,ordermanagement.status,ordermanagement.createdby,ordermanagement.createdat,ordermanagement.updatedby,ordermanagement.updatedat,ordermanagement.refiltype,ordermanagement.deldate,ordermanagement.cylinderimg,users.firstname,useraddresses.addressline1,useraddresses.pincode,useraddresses.latitude,useraddresses.longitude,vendors.businessname,products.cylindertypeid,products.cylinderweightid,products.location,products.price,products.petroleumid,products.proimg FROM ordermanagement left join users on users.id=ordermanagement.userid left join useraddresses on useraddresses.id=ordermanagement.deliveryaddressid left join vendors on vendors.id=ordermanagement.vendorid left join products on products.id=ordermanagement.productid where ordermanagement.status=1 AND ordermanagement.userid='$userid'");
+}elseif($status==2){
+
+  $sql=pg_query($db,"SELECT ordermanagement.id,ordermanagement.userid,ordermanagement.deliveryaddressid,ordermanagement.vendorid,ordermanagement.productid,ordermanagement.quantity,ordermanagement.status,ordermanagement.createdby,ordermanagement.createdat,ordermanagement.updatedby,ordermanagement.updatedat,ordermanagement.refiltype,ordermanagement.deldate,ordermanagement.cylinderimg,users.firstname,useraddresses.addressline1,useraddresses.pincode,useraddresses.latitude,useraddresses.longitude,vendors.businessname,products.cylindertypeid,products.cylinderweightid,products.location,products.price,products.petroleumid,products.proimg FROM ordermanagement left join users on users.id=ordermanagement.userid left join useraddresses on useraddresses.id=ordermanagement.deliveryaddressid left join vendors on vendors.id=ordermanagement.vendorid left join products on products.id=ordermanagement.productid where ordermanagement.status=2 AND ordermanagement.userid='$userid'");
+}else{
+
+$sql=pg_query($db,"SELECT ordermanagement.id,ordermanagement.userid,ordermanagement.deliveryaddressid,ordermanagement.vendorid,ordermanagement.productid,ordermanagement.quantity,ordermanagement.status,ordermanagement.createdby,ordermanagement.createdat,ordermanagement.updatedby,ordermanagement.updatedat,ordermanagement.refiltype,ordermanagement.deldate,ordermanagement.cylinderimg,users.firstname,useraddresses.addressline1,useraddresses.pincode,useraddresses.latitude,useraddresses.longitude,vendors.businessname,products.cylindertypeid,products.cylinderweightid,products.location,products.price,products.petroleumid,products.proimg FROM ordermanagement left join users on users.id=ordermanagement.userid left join useraddresses on useraddresses.id=ordermanagement.deliveryaddressid left join vendors on vendors.id=ordermanagement.vendorid left join products on products.id=ordermanagement.productid where ordermanagement.status=0 AND ordermanagement.userid='$userid'");
+}
 $my=array();
  if($sql){
-   //var_dump($sql); 
     while($sql2=pg_fetch_assoc($sql)){
  
              http_response_code(201);
