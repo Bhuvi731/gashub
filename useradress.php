db7fb54ac0e181a41537f20830b96e791477cf04
Bhuvi731 <105772345+Bhuvi731@users.noreply.github.com>
2022-06-28 15:52:33 +0530

    Update useradress.php

--- useradress.php
+++ useradress.php
@@ -136,7 +136,7 @@ while($row=pg_fetch_assoc($vendor))
   ?>
 <tr>
 <td><?php echo $i?></td>
- <td><?php echo $row['fristname']?></td>
+ <td><?php echo $row['firstname']?></td>
 <td><?php echo $row['addressline1'] ?></td>
 <td><?php echo $row['pincode'] ?></td>
 
