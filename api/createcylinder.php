36c707377a98e39f84f0cd3f1cc164807965f674
arokiaani2423 <arokianani2423@gmail.com>
2022-07-04 11:45:08 +0530

    Update createcylinder.php

--- api/createcylinder.php
+++ api/createcylinder.php
@@ -12,20 +12,29 @@ if (isset($_POST['petroleumid'])) {
     $status = "1";
     $createdby = "1";
     $createdat = "1";
-    if (!empty($petroleumid)  && !empty($status)) {
-        $sql = "INSERT INTO cylinder(petroleumid,weight,status,createdby,createdat)VALUES('$petroleumid','$weight','$status','$createdby','$createdat')";
-        $query = pg_query($db, $sql);
-        if ($query) {
-            if ($sql) {
+    if (!empty($petroleumid) && !empty($weight) && !empty($status)) {
+        $sql = "SELECT id FROM cylinder WHERE weight='$weight'AND petroleumid='$petroleumid'";
+        $res = pg_query($db, $sql);
+        if (pg_num_rows($res) > 0) {
+            if ($sql3 = pg_fetch_array($res)) {
 
-                echo "success";
+                echo "weight_already_existed";
+            }
+        } else {
+            $sql = "INSERT INTO cylinder(petroleumid,weight,status,createdby,createdat)VALUES('$petroleumid','$weight','$status','$createdby','$createdat')";
+            $query = pg_query($db, $sql);
+            if ($query) {
+                if ($sql) {
+
+                    echo "success";
+                } else {
+                    echo "error";
+                }
             } else {
                 echo "error";
             }
-        } else {
-            echo "error";
         }
+    } else {
+        echo "Error Please Check.";
     }
-} else {
-    echo "Error Please Check.";
 }
