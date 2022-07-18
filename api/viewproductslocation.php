7b1d4e7b0745769b1c5bd1993a2e5b234f68287a
Bhuvi731 <105772345+Bhuvi731@users.noreply.github.com>
2022-07-19 05:21:20 +0530

    viewproductslocation.php

---
+++ api/viewproductslocation.php
@@ -0,0 +1,83 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header('Content-Type: text/plain');
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+include_once '../database/db.php';
+if(isset($_POST['longitude']) && ($_POST['latitude']))
+{
+
+
+ $latitudeFrom =$_POST['longitude'];
+ $longitudeFrom =$_POST['latitude'];
+$sql=pg_query($db,"SELECT * FROM vendoraddresses where vendoraddresses.status=1");
+
+$my=array();
+$my1=array();
+$my2=array();
+ if($sql){
+   //var_dump($sql); 
+    while(($sql2=pg_fetch_assoc($sql))){
+
+     $my[]=$sql2;
+}
+    $longlen=count($my);
+    foreach($my as $i => $i_value) {
+
+    	$latitudeTo=$i_value['latitude'];
+    	$longitudeTo=$i_value['longitude'];
+
+
+if($latitudeTo!=null && $longitudeTo!=null)
+{
+
+
+    	$theta = $longitudeFrom - $longitudeTo;
+$dist = sin(deg2rad($latitudeFrom)) * sin(deg2rad($latitudeTo)) +  cos(deg2rad($latitudeFrom)) * cos(deg2rad($latitudeTo)) * cos(deg2rad($theta));
+$dist = acos($dist);
+ $dist = rad2deg($dist);
+ $miles = $dist * 60 * 1.1515;
+  $distance = ($miles * 1.609344);
+ //echo"'<br/>'";
+
+if($distance<=3){
+
+    $fristaddid=$i_value['id'];
+
+  $sql=pg_query($db,"SELECT * from cylinder LEFT JOIN petroleum ON petroleum.id=cylinder.petroleumid LEFT JOIN  vendors ON vendors.id=cylinder.vendorid LEFT JOIN vendoraddresses ON vendoraddresses.id=vendors.id WHERE cylinder.vendoraddid='$fristaddid' AND cylinder.status=1");
+
+  if($sql){
+    $sql2=pg_fetch_assoc($sql);
+
+            
+    $my1[] = $sql2;
+  
+
+}
+}
+elseif($distance>3 && $distance<=5){
+
+$secondaddid=$i_value['id'];
+
+  $sql1=pg_query($db,"SELECT * from cylinder LEFT JOIN petroleum ON petroleum.id=cylinder.petroleumid LEFT JOIN  vendors ON vendors.id=cylinder.vendorid LEFT JOIN vendoraddresses ON vendoraddresses.id=vendors.id WHERE cylinder.vendoraddid='$secondaddid' AND cylinder.status=1");
+
+  if($sql1){
+    $sql2=pg_fetch_assoc($sql1);
+       $my2[]= $sql2;
+            
+    
+}
+}
+}
+}
+}
+echo json_encode($my1);
+echo json_encode($my2);
+
+}else{
+	echo"error";
+}
+
+?>
