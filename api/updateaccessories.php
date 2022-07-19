fb56473210d4fab2bbdf482a0d7fde9a33a2223c
Bhuvi731 <105772345+Bhuvi731@users.noreply.github.com>
2022-07-19 05:46:29 +0530

    acccess and inventory

--- api/updateaccessories.php
+++ api/updateaccessories.php
@@ -6,25 +6,30 @@
 // header("Access-Control-Max-Age: 3600");
 // header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 include_once '../database/db.php';
-if (isset($_GET['id'])) {
-    $id = $_GET['id'];
-    $accessoriesname = $_GET['accessoriesname'];
-    $status = "1";
-    $updatedby = "1";
-    $updatedat = date("d-m-y");
-    if (isset($id) && isset($accessoriesname) && isset($status)) {
-        $sql = "UPDATE accessories SET accessoriesname='$accessoriesname',status='$status',updatedby='$updatedby',updatedat='$updatedat' WHERE id='$id'";
-        $query = pg_query($db, $sql);
-        if ($query) {
-
-
-            echo  "success";
-        } else {
-
-            echo "Error";
-        }
+if(isset($_GET['id']))
+{
+$id=$_GET['id'];
+$accessoriesname=$_GET['accessoriesname'];
+$status="1";    
+$updatedby="1";
+$updatedat=date("d-m-y");                                                                                                                                                                                    
+ if(isset($id) && isset($accessoriesname) && isset($status)){
+    $sql = "UPDATE accessories SET accessoriesname='$accessoriesname',status='$status',updatedby='$updatedby',updatedat='$updatedat' WHERE id='$id'";
+    $query=pg_query($db,$sql);
+    if($query)
+    {
+      
+                 
+        echo  "success";
+    }else
+    {
+              
+        echo "Error";
     }
-} else {
-
+}
+}
+else
+{
+      
     echo "Error Please Check";
 }
