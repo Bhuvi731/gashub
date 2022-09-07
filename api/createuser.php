ea7ee7a8e526946e7dca2d33cba338ba9e2a4c01
bhuvi@731 <bhuviarasu731@gmail.com>
2022-09-07 12:06:20 +0530

    Update createuser.php

--- api/createuser.php
+++ api/createuser.php
@@ -6,39 +6,37 @@ header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 include_once '../database/db.php';
-$firstname=$_POST['firstname'];
-$lastname=$_POST['lastname'];
-$phone=$_POST['phone'];
-$email=$_POST['email'];
-$password=$_POST['password'];
-$gender=$_POST['gender'];
-$dateofbirth=$_POST['dateofbirth'];
-$status="1";
-$createdby="1";                                                                                                                                                                                         
-if(!empty($firstname) && !empty($phone) && !empty($email) &&
-!empty($gender) && !empty($dateofbirth) &&
-!empty($status) && !empty($password))
-{ 
+echo $firstname = $_POST['firstname'];
+echo $lastname = $_POST['lastname'];
+echo $phone = $_POST['phone'];
+echo $email = $_POST['email'];
+echo $password = $_POST['password'];
+echo $gender = $_POST['gender'];
+echo $dateofbirth = $_POST['dateofbirth'];
+$status = "1";
+$createdby = "1";
+if (
+  !empty($firstname) && !empty($phone) && !empty($email) &&
+  !empty($gender) && !empty($dateofbirth) &&
+  !empty($status) && !empty($password)
+) {
   $sql = "SELECT * FROM users WHERE email='$email'";
-    $res = pg_query($db, $sql);
-    if(pg_num_rows($res) > 0){
-        http_response_code(201);         
-        echo"email_existed";
-      }else{
+  $res = pg_query($db, $sql);
+  if (pg_num_rows($res) > 0) {
+    http_response_code(201);
+    echo "email_existed";
+  } else {
     $sql = "INSERT INTO users(firstname,lastname,phone,email,gender,dateofbirth,status,createdby,password)VALUES('$firstname','$lastname','$phone','$email','$gender','$dateofbirth','$status','$createdby','$password')RETURNING id";
-    $query=pg_query($db,$sql);
-    if($query)
-    {
-        http_response_code(201);         
-        echo "Success";
-    }else
-    {
-        http_response_code(503);        
-        echo"Error";
+    $query = pg_query($db, $sql);
+    if ($query) {
+      http_response_code(201);
+      echo "Success";
+    } else {
+      http_response_code(503);
+      echo "Error";
     }
   }
- }
- else{
-    http_response_code(400);    
-    echo "Error Please Check.";
-   }
+} else {
+  http_response_code(400);
+  echo "Error Please Check.";
+}
